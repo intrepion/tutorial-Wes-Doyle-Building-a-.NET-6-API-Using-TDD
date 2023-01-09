f627b14cd0f1bdb891559dff2b2c086faee78504
Oliver Forral <intrepion@gmail.com>
2023-01-09 11:03:40 -0700

    red

---
+++ CloudCustomers/CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
@@ -0,0 +1,33 @@
+using CloudCustomers.API.Models;
+using CloudCustomers.API.Services;
+using CloudCustomers.UnitTests.Fixtures;
+using CloudCustomers.UnitTests.Helpers;
+using Moq;
+using Moq.Protected;
+
+namespace CloudCustomers.UnitTests.Systems.Services;
+
+public class TestUsersService
+{
+    [Fact]
+    public async Task GetAllUsers_WhenCalled_InvokesHttpGetRequest() {
+        // Arrange
+        var expectedResponse = UsersFixtures.GetTestUsers;
+        var handlerMock = MockHttpMessageHandler<User>.SetupBasicGetResourceList(expectedResponse);
+        var httpClient = new HttpClient(handlerMock.Object);
+        var sut = new UsersService(httpClient);
+
+        // Act
+        await sut.GetAllUsers();
+
+        // Assert
+        handlerMock
+            .Protected()
+            .Verify(
+                "SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get),
+                ItExpr.IsAny<CancellationToken>()
+            );
+    }
+}
