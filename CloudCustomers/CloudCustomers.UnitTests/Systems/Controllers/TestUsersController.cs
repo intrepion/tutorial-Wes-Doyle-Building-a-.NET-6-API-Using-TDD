3da10171284b4aeb6a58848ff5eac2b32dff43d4
Oliver Forral <intrepion@gmail.com>
2023-01-09 01:56:44 -0700

    red

---
+++ CloudCustomers/CloudCustomers.UnitTests/Systems/Controllers/TestUsersController.cs
@@ -0,0 +1,10 @@
+namespace CloudCustomers.UnitTests.Systems.Controllers;
+
+public class TestUsersController
+{
+    [Fact]
+    public void Get_OnSuccess_ReturnsStatusCode200()
+    {
+        var sut = new UsersController();
+    }
+}
